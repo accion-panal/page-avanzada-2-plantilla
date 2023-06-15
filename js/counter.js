9de4b0405c2ad2b1c15caeacaa5c16a11341d278
JaackX1 <nicolascabreramillacan@gmail.com>
2023-06-15 16:52:52 -0400

    fix: js countUp addition

--- js/counter.js
+++ js/counter.js
@@ -2,7 +2,7 @@
 
 	'use trict';
 // How long you want the animation to take, in ms
-const animationDuration = 2000;
+const animationDuration = 4500;
 // Calculate how long each ‘frame’ should last if we want to update the animation 60 times per second
 const frameDuration = 1000 / 60;
 // Use that to calculate how many frames we need to complete the animation
--- js/utils/counts-cant-prop.js
+++ js/utils/counts-cant-prop.js
@@ -1,19 +1,31 @@
 import { getProperties } from "../services/PropertiesServices.js";
+import { PropertyData, limitDataApi } from "../Data/userId.js";
 
-export default async function countProp() {
-  const response = await getProperties(1, 10, 0, 1, 1, 0);
-  const data = response.data;
 
-  document.getElementById("count-total-prop").innerHTML = `<span class="number"><span class="countup" style="font-weight: bold;font-size: 50px; color: #999999;">${response.meta.totalItems}</span>
-	</span>
-  <span class="caption text-black-50">Total de Propiedades</span>`;
+const countsProps = async () => {
+    const {CodigoUsuarioMaestro, companyId, realtorId} = PropertyData;
+    const response = await getProperties(1, limitDataApi.limit, CodigoUsuarioMaestro, 1, companyId, realtorId);
+    const data = response.data;
 
-  document.getElementById("count-sale-prop").innerHTML = `<span class="number"><span class="countup" style="font-weight: bold;font-size: 50px; color: #999999;">298</span>
+    document.getElementById("count-total-prop").innerHTML = `
+        <div class="row section-counter  ">
+            <div class="counter-wrap mb-5 mb-lg-0">
+                <span class="number">
+                    <span class="countup" style="font-weight: bold;font-size: 60px; color: #999999;">${ response.meta.totalItems }</span>
+                </span>
+                <span class="caption text-black-50">Total de Propiedades</span>
+            </div>
+        </div>
+    `;
+
+    /* document.getElementById("count-sale-prop").innerHTML = `<span class="number"><span class="countup" style="font-weight: bold;font-size: 50px; color: #999999;">298</span>
 	</span>
   <span class="caption text-black-50">Propiedades Vendidas</span>`;
 
-  document.getElementById("count-lease-prop").innerHTML = `<span class="number"><span class="countup" style="font-weight: bold;font-size: 50px; color: #999999;">181</span>
+    document.getElementById("count-lease-prop").innerHTML = `<span class="number"><span class="countup" style="font-weight: bold;font-size: 50px; color: #999999;">181</span>
 	</span>
-  <span class="caption text-black-50">Propiedades Arrendadas</span>`;
+  <span class="caption text-black-50">Propiedades Arrendadas</span>`; */
 
 }
+
+countsProps();
